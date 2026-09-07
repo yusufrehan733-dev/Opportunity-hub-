8e2e4e8b41a49d3d30e9459601ccdae9f2779abf
yusufrehan733-dev <yusufrehan733@gmail.com>
2026-09-07 09:50:15 +0500

    Add files via upload

---
+++ api-server/src/components/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './pages/index';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>
+);
