9f88ec15e3834dcb748160865ed1d9c9ea3c4751
yusufrehan733-dev <yusufrehan733@gmail.com>
2026-09-13 09:10:22 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
@@ -9,9 +10,13 @@ const root = document.getElementById("root");
 if (!root) {
   document.body.innerHTML = "<h1>Root not found ❌</h1>";
 } else {
+  const queryClient = new QueryClient();
+
   ReactDOM.createRoot(root).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
