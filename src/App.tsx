dc6575a9c369a98a0ac418cf5e4bc048a8d713a3
yusufrehan733-dev <yusufrehan733@gmail.com>
2026-09-15 23:11:13 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
+import Register from "./Register";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="*" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
