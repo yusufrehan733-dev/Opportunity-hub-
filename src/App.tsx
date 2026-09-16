67f428675c3ab8b81279d362daa8ebc78147f2e5
yusufrehan733-dev <yusufrehan733@gmail.com>
2026-09-16 06:05:40 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
+import ResetPassword from "./ResetPassword";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
 
       <Route path="/" element={<Navigate to="/login" replace />} />
 
