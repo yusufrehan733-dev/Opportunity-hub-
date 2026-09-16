f3e2e43dba1ce6e9e29d3cd583fb28ecce81fd66
yusufrehan733-dev <yusufrehan733@gmail.com>
2026-09-16 22:02:35 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import ResetPassword from "./ResetPassword";
+import InviteRegister from "./InviteRegister";
 
 import Dashboard from "./pages/Dashboard";
 import Skills from "./pages/Skills";
@@ -19,6 +20,10 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route
+        path="/invite-register/:token"
+        element={<InviteRegister />}
+      />
 
       {/* DASHBOARD */}
       <Route path="/dashboard" element={<Dashboard />} />
